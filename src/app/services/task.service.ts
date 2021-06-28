8af83af8580276cfa9aab3079c9baeacd98b62db
Robert Bowen <rbowen@pbaco.org>
2021-06-28 09:27:47 -0400

    Able to perminently make changes to db

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http'; 
 import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
-import { Observable} from 'rxjs'
+import { Observable} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +25,10 @@ export class TaskService {
   deleteTask(task:Task): Observable<Task>{
     const url = `${this.apiurl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task:Task): Observable<Task>{
+    const url = `${this.apiurl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
   }
 }

9343bf76523bddca0fe9467d3ca9f21f6a8e7ef3
Robert <rbowensoftware@gmail.com>
2021-07-12 23:53:19 -0400

    Issue with initializing subscription

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UiService} from '../../services/ui.service';
+import {Subscription} from 'rxjs'
 
 @Component({
   selector: 'app-header',
@@ -8,8 +10,10 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title: string = 'Task Tracker';
+  showAddTask: boolean = true;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) { }
 
   ngOnInit(): void {
   }
